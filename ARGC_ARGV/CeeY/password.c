612ccbad256b3e0cd90e03164caa12468f8327b3
ceeygithub <cynthiaekhomwandolor@gmail.com>
2023-05-21 07:37:00 +0100

    corrected assignment

--- ARGC_ARGV/CeeY/password.c
+++ ARGC_ARGV/CeeY/password.c
@@ -1,27 +1,28 @@
-::nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /**
- *main- entry point
- *Return: always 0
- */
+  5   5  *main- entry point
+  6   6  *Return: always 0
+  7   7  */
 
 int main(int argc ,char* argv[])
 {
 	int i;
-	char password[] = "secrete";
-
-	if (strcmp(argv[1], password) == 0)
+	char password[] = "Cohort 14";
+	if (argc < 2 || argc > 2)
 	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%s\n", argv[i]);
-		}
-		printf("\n");
+		printf("Error! Enter 2 arguments: i.e ./test correctPassword\n");
+	}
+
+	else if (strcmp(argv[1], password) == 0)
+	{
+
+		printf("Welcome, Password Success!\nTask passed!!!");
 	}
 	else
 	{
-		printf(" you have no access,please enter correct password");
+		printf("Password Protected!!!\nPlease enter the correct password in this format: ./test correctPassw    ord \n");
 	}
 	return (0);
 }
